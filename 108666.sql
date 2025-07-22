23f810fe18ddcbe653e7e49c424986b287f6da64
leyin <3503604459@qq.com>
2025-07-23 00:14:01 +0800

    1

--- 108666.sql
+++ 108666.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Dump SQL
 
- Source Server         : 108pq
+ Source Server         : 098_JDBC
  Source Server Type    : MySQL
  Source Server Version : 50727 (5.7.27)
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50727 (5.7.27)
  File Encoding         : 65001
 
- Date: 19/07/2025 14:59:53
+ Date: 22/07/2025 23:56:56
 */
 
 SET NAMES utf8mb4;
@@ -29,26 +29,26 @@ CREATE TABLE `answer_record`  (
   `user_answer_index` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `correct` tinyint(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of answer_record
 -- ----------------------------
-INSERT INTO `answer_record` VALUES (2, 1, 101, 101, '1', 1);
-INSERT INTO `answer_record` VALUES (3, 1, 102, 102, '3', 1);
-INSERT INTO `answer_record` VALUES (4, 1, 103, 103, '3', 0);
-INSERT INTO `answer_record` VALUES (5, 1, 104, 104, '3', 0);
-INSERT INTO `answer_record` VALUES (6, 1, 105, 105, '3', 0);
-INSERT INTO `answer_record` VALUES (7, 3, 101, 101, '1', 1);
-INSERT INTO `answer_record` VALUES (8, 3, 102, 102, '3', 1);
-INSERT INTO `answer_record` VALUES (9, 3, 103, 103, '1', 1);
-INSERT INTO `answer_record` VALUES (10, 3, 104, 104, '1', 1);
-INSERT INTO `answer_record` VALUES (11, 3, 105, 105, '0', 1);
-INSERT INTO `answer_record` VALUES (12, 1, 101, 101, '1', 1);
-INSERT INTO `answer_record` VALUES (13, 1, 102, 102, '3', 1);
-INSERT INTO `answer_record` VALUES (14, 1, 103, 103, '1', 1);
-INSERT INTO `answer_record` VALUES (15, 1, 104, 104, '2', 0);
-INSERT INTO `answer_record` VALUES (16, 1, 105, 105, '3', 0);
+INSERT INTO `answer_record` VALUES (78, 1, 101, 101, '1', 1);
+INSERT INTO `answer_record` VALUES (79, 1, 102, 102, '3', 1);
+INSERT INTO `answer_record` VALUES (80, 1, 103, 103, '1', 1);
+INSERT INTO `answer_record` VALUES (81, 1, 104, 104, '1', 1);
+INSERT INTO `answer_record` VALUES (82, 1, 105, 105, '0', 1);
+INSERT INTO `answer_record` VALUES (88, 2, 101, 101, '0', 0);
+INSERT INTO `answer_record` VALUES (89, 2, 102, 102, '0', 0);
+INSERT INTO `answer_record` VALUES (90, 2, 103, 103, '0', 0);
+INSERT INTO `answer_record` VALUES (91, 2, 104, 104, '0', 0);
+INSERT INTO `answer_record` VALUES (92, 2, 105, 105, '1', 0);
+INSERT INTO `answer_record` VALUES (93, 3, 101, 101, '1', 1);
+INSERT INTO `answer_record` VALUES (94, 3, 102, 102, '3', 1);
+INSERT INTO `answer_record` VALUES (95, 3, 103, 103, '1', 1);
+INSERT INTO `answer_record` VALUES (96, 3, 104, 104, '0', 0);
+INSERT INTO `answer_record` VALUES (97, 3, 105, 105, '1', 0);
 
 -- ----------------------------
 -- Table structure for course
@@ -67,7 +67,7 @@ CREATE TABLE `course`  (
 -- ----------------------------
 -- Records of course
 -- ----------------------------
-INSERT INTO `course` VALUES (1, 'wanglaoshi', '中国近代史', '101', '2025-07-14 12:20:25', 100);
+INSERT INTO `course` VALUES (1, 'wanglaoshi', '中国近代史', '101', '2025-07-22 23:20:25', 100);
 INSERT INTO `course` VALUES (2, 'wanglaoshi', 'vue基础', '102', '2025-07-14 17:20:54', 120);
 
 -- ----------------------------
@@ -92,7 +92,7 @@ CREATE TABLE `discussions`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `feedback`;
 CREATE TABLE `feedback`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `qid` int(11) NULL DEFAULT NULL,
   `uid` int(11) NULL DEFAULT NULL,
   `too_fast` int(11) NULL DEFAULT NULL,
@@ -102,11 +102,13 @@ CREATE TABLE `feedback`  (
   `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of feedback
 -- ----------------------------
+INSERT INTO `feedback` VALUES (2, 1, 1, 1, 1, 1, 1, '11111111', '2025-07-19 17:48:26');
+INSERT INTO `feedback` VALUES (3, 1, 3, 1, 0, 0, 0, '1', '2025-07-19 17:55:05');
 
 -- ----------------------------
 -- Table structure for question
@@ -127,11 +129,31 @@ CREATE TABLE `question`  (
 -- ----------------------------
 -- Records of question
 -- ----------------------------
-INSERT INTO `question` VALUES (101, '中国的首都是哪座城市？', '上海', '北京', '深圳', '重庆', 'B', 1, 1);
-INSERT INTO `question` VALUES (102, '2 + 2 = ?', '1', '2', '3', '4', 'D', 1, 1);
+INSERT INTO `question` VALUES (101, '中国的首都是哪座？', '上海', '北京', '深圳', '重庆', 'B', 1, 0.666667);
+INSERT INTO `question` VALUES (102, '2 + 2 = ?', '1', '2', '3', '4', 'D', 1, 0.666667);
 INSERT INTO `question` VALUES (103, '下列哪项是编程语言？', 'vue', 'python', 'css', 'html', 'B', 1, 0.666667);
 INSERT INTO `question` VALUES (104, '地球上最大的动物是？', '大象', '蓝鲸', '老鼠', '麻雀', 'B', 1, 0.333333);
 INSERT INTO `question` VALUES (105, '太阳从哪边升起？', '东', '南', '西', '北', 'A', 1, 0.333333);
+
+-- ----------------------------
+-- Table structure for rank
+-- ----------------------------
+DROP TABLE IF EXISTS `rank`;
+CREATE TABLE `rank`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NULL DEFAULT NULL,
+  `qid` int(11) NULL DEFAULT NULL,
+  `accuracy` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ranking` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of rank
+-- ----------------------------
+INSERT INTO `rank` VALUES (13, 1, 1, '100.00', '1');
+INSERT INTO `rank` VALUES (14, 3, 1, '60.00', '2');
+INSERT INTO `rank` VALUES (16, 2, 1, '0.00', '3');
 
 -- ----------------------------
 -- Table structure for user
