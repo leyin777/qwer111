ce2f6faf326f583c97a9e76a8bb15fe846328abd
wangbingchen6699 <3120560278@QQ.com>
2025-07-26 02:45:33 +0800

    Delete pqbackmain directory

--- pqbackmain/src/main/java/com/example/mapper/CourseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.Course;
-
-public interface CourseMapper extends BaseMapper<Course> {
-}
