54c23c7bf48a347a5bcd63d978c8fd18b174c2ca
wangbingchen6699 <3120560278@QQ.com>
2025-07-26 02:53:08 +0800

    Delete pqbackmain directory

--- pqbackmain/src/main/java/com/example/mapper/QuestionGroupMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface QuestionGroupMapper {
-    Long getQidByFileId(@Param("fileId") Long fileId);
-} 
